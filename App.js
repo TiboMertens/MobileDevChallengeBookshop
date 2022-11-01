e741dfe30c9cb2cf22baac000e7b674cb1ff8ce9
TiboMertens <tibomertens25@gmail.com>
2022-11-01 13:41:51 +0100

    added title

--- App.js
+++ App.js
@@ -1,22 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <StatusBar style="auto" />
+      </View>
+      <View style={styles.heading}>
+        <View style={styles.title}>
+          <Text style={styles.h1}>Bookshop</Text>
+        </View>
+        <View style={styles.shoppingCart}>
+          <Text style={styles.shoppingCartAmount}>1</Text>
+        </View>
+      </View>
     </View>
-    //hey
-    //hey 2.0
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container: {},
+  h1: {
+    fontSize: 32,
+    fontWeight: "bold",
+    marginTop: 60,
+    marginLeft: 20,
+  },
+  heading: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  shoppingCartAmount: {
+    marginTop: 90,
+    fontSize: 18,
   },
 });
